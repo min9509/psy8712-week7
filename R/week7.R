792a87f3ef0f4002ba9f603a771a407b20b3efa3
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-09 20:15:10 -0600

    Part 2 – Data Import and Cleaning - Object 7 (3)

--- R/week7.R
+++ R/week7.R
@@ -8,6 +8,7 @@ library(tidyverse)
 week7_tbl <- read.csv("data/week3.csv", header = TRUE) %>%
   mutate(timeStart = ymd_hms(timeStart)) %>%
   mutate(timeEnd = ymd_hms(timeEnd)) %>%
-  mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control")))
+  mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
+  mutate(gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female")))
 
   
