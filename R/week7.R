ca643d04dcd8ee9161199f9ba23570dc9ceffaf9
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-09 20:20:58 -0600

    Part 2 – Data Import and Cleaning - Finish

--- R/week7.R
+++ R/week7.R
@@ -11,7 +11,10 @@ week7_tbl <- read.csv("data/week3.csv", header = TRUE) %>%
   mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
   mutate(gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female"))) %>%
   filter(q6 == 1) %>%
-  select(-q6)
+  select(-q6) %>%
+  mutate(timeSpent = difftime(timeEnd, timeStart, units =c("mins")))
+
+
 
 
   
