f4b5a4f31751a849f23bfb759bdd83a0e06c4eeb
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-09 20:10:18 -0600

    Part 2 – Data Import and Cleaning - object 6 and 7

--- R/week7.R
+++ R/week7.R
@@ -1,1 +1,10 @@
 # Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+
+
+# Data Import and Cleaning
+week7_tbl <- read.csv("data/week3.csv", header = TRUE) %>%
+  mutate(timeStart = ymd_hms(timeStart)) %>%
+  mutate(timeEnd = ymd_hms(timeEnd))
